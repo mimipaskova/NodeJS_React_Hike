424fcded055576f51a44b5c2d3d52bd332921739
Maria Paskova <maria.paskova@mentormate.com>
2017-06-29 19:51:38 +0300

    move style to .css file

--- client/src/components/story.js
+++ client/src/components/story.js
@@ -2,28 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import GoogleMapReact from 'google-map-react';
 
-const K_WIDTH = 40;
-const K_HEIGHT = 40;
-const greatPlaceStyle = {
-  // initially any map object has left top corner at lat lng coordinates
-  // it's on you to set object origin to 0,0 coordinates
-  position: 'absolute',
-  width: K_WIDTH,
-  height: K_HEIGHT,
-  left: -K_WIDTH / 2,
-  top: -K_HEIGHT / 2,
-
-  border: '5px solid #f44336',
-  borderRadius: K_HEIGHT,
-  backgroundColor: 'white',
-  textAlign: 'center',
-  color: '#3f51b5',
-  fontSize: 16,
-  fontWeight: 'bold',
-  padding: 4
-};
-
-const Pin = ({ text }) => <div className='pin-map'  style={greatPlaceStyle}>{text}</div>;
+const Pin = ({ text }) => <div className='pin-map' >{text}</div>;
 
 class Story extends Component {
 
